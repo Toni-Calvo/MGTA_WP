c4b13066f10a414065c553c66e4a69bea721e49e
Toni Calvo <tonicb23mj@gmail.com>
2024-10-17 13:13:36 +0200

    initialize wp

---
+++ wp3.py
@@ -0,0 +1,20 @@
+import numpy as np
+from wp1 import main, isInECAC, getDistance, getSlots, plotSlotsArrOverTime
+
+# --------------------------------------------------------------------------------------------
+# GLOBAL VARIABLES
+Hstart = 6 # h
+
+# --------------------------------------------------------------------------------------------
+# FUNCTIONS
+
+def filterFPs(fpDic, Hstart, HnoReg):
+    """Filters all the flightplans to only show the ones between Hstart and HnoReg."""
+    filteredFPs = {}
+    for key in fpDic:
+        if fpDic.get(key) != None:
+            if Hstart * 60 < (int(key.split(':')[0]) * 60 + int(key.split(':')[1])) <= (HnoReg[0] * 60 + HnoReg[1]):
+                filteredFPs.update({key : fpDic.get(key)})
+    
+    return filteredFPs
+
